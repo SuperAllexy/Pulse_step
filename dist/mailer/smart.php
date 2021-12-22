4e9b453716bc2fdfd47e7343030198846605c575
Alex <alex94alex03@gmail.com>
2021-12-22 16:01:20 +0200

    master

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'alex94alex03@gmail.com';                 // Наш логин
-$mail->Password = 'kgutxvmjyhzkpbec';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('alex94alex03@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('alex94alex03@gmail.com');     // Add a recipient
+$mail->setFrom('', 'Pulse');   // От кого письмо 
+$mail->addAddress('alex94alex@mail.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
